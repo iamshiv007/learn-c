9ed8e5b95fd53bf41148b9c41d7ea5f3af5e62d0
iamshiv007 <iamshiv20032003@gmail.com>
2023-08-28 11:32:21 +0530

    sum of numbers given by user

--- sum.c
+++ sum.c
@@ -1,14 +1,64 @@
-// Online C compiler to run C program online
+// Sum of to numbers
+// #include <stdio.h>
+
+// int main() {
+
+//     int num1 = 10;
+//     int num2 = 20;
+//     int sum = num1 + num2;
+
+//     // Write C code here
+//     printf("The sum of %d and %d is %d\n", num1, num2, sum);
+
+//     return 0;
+// }
+
+
+// Sum of numbers given by user
+
+// #include <stdio.h>
+
+// int main()
+// {
+//     int numCount, i;
+//     int total = 0;
+
+//     printf("Enter the count of numbers: ");
+//     scanf("%d", &numCount);
+
+//     printf("Enter %d numbers:\n", numCount);
+
+//     for (i = 0; i < numCount; i++)
+//     {
+//         int num;
+//         scanf("%d", &num);
+//         total += num;
+//     }
+
+//     printf("The sum of the entered numbers is: %d\n", total);
+
+//     return 0;
+// }
+
+
+// Sum of all numbers given by user
 #include <stdio.h>
 
 int main() {
-    
-    int num1 = 10;
-    int num2 = 20;
-    int sum = num1 + num2;
-    
-    // Write C code here
-    printf("The sum of %d and %d is %d\n", num1, num2, sum);
+    int num, total = 0;
+
+    printf("Enter numbers separated by spaces (end with -1):\n");
+
+    do{
+        scanf("%d", &num);
+        
+       if(num != -1){
+        total += num;
+       }
+       
+    } while(num != -1);
+
+    printf("The sum of the entered numbers is: %d\n", total);
 
     return 0;
 }
