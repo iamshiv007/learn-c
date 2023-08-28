bef6583bb2adf0e34dd512ea6c8ac0e087e736f8
iamshiv007 <iamshiv20032003@gmail.com>
2023-08-28 10:26:54 +0530

    sum of two numbers

---
+++ sum.c
@@ -0,0 +1,14 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    
+    int num1 = 10;
+    int num2 = 20;
+    int sum = num1 + num2;
+    
+    // Write C code here
+    printf("The sum of %d and %d is %d\n", num1, num2, sum);
+
+    return 0;
+}
